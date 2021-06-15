053b3e1727386ca3714b0feb24d0020846a6aaed
Jesus <manzanocaminojesus@gmail.com>
2021-06-15 23:27:13 +0200

    Bump dependencies:  - Compose -> 1.0.0-beta08  - Kotlin -> 1.5.10  - Gradle plugin -> 7.1.0-alpha02  - AndroidX core -> 1.5.0  - AndroidX Appcompat -> 1.3.0  - AndroidX Paging -> 3.1.0-alpha01  - AndroidX Activity -> 1.3.0-beta01  - Coil (image library) -> 1.2.1  - Compose Paging -> 1.0.0-alpha10  - Accompanist -> 0.11.1  - Kotlin coroutines -> 1.4.3  - AndroidX compose activity -> 1.3.0-beta01

--- app/src/main/java/com/marvelsample/app/ui/base/compose/CharacterImage.kt
+++ app/src/main/java/com/marvelsample/app/ui/base/compose/CharacterImage.kt
@@ -2,14 +2,16 @@ package com.marvelsample.app.ui.base.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.google.accompanist.coil.rememberCoilPainter
+import com.google.accompanist.imageloading.ImageLoadState
 import com.marvelsample.app.R
-import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun Image(
@@ -18,21 +20,27 @@ fun Image(
     modifier: Modifier = Modifier,
     contentDescription: String? = null
 ) {
-    CoilImage(
-        data = imageUrl ?: R.drawable.ic_baseline_face_24,
+    val painter = rememberCoilPainter(imageUrl)
+    Image(
+        painter = painter,
         modifier = modifier,
         contentScale = ContentScale.Crop,
-        loading = {
-            Box(Modifier.matchParentSize()) {
+        contentDescription = contentDescription ?: "$imageLabel image."
+    )
+
+    when (painter.loadState) {
+        is ImageLoadState.Loading -> {
+            // Display a circular progress indicator whilst loading
+            Box(Modifier.fillMaxSize()) {
                 CircularProgressIndicator(Modifier.align(Alignment.Center))
             }
-        },
-        error = {
+        }
+        is ImageLoadState.Error -> {
+            // If you wish to display some content if the request fails
             Image(
                 painter = painterResource(R.drawable.ic_baseline_face_24),
                 contentDescription = contentDescription ?: "Image $imageLabel error image."
             )
-        },
-        contentDescription = contentDescription ?: "$imageLabel image."
-    )
+        }
+    }
 }
